1b578f1d1b6d774e71803246527b3e867ee638f1
Tanmay <khandelwaltanmay42@gmail.com>
2026-07-13 23:51:40 +0530

    Time: 0 ms (100%), Space: 12.9 MB (48.01%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL) return true;
+        if (p == NULL || q == NULL) return false;
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+        q1.push(p);
+        q2.push(q);
+        while (!q1.empty() && !q2.empty()) {
+            int size1 = q1.size();
+            int size2 = q2.size();
+            if (size1 != size2) {
+                return false;
+            }
+            for (int i = 0; i < size1; i++) {
+                TreeNode* temp1 = q1.front(); 
+                q1.pop();
+                TreeNode* temp2 = q2.front(); 
+                q2.pop();
+                if (temp1->val != temp2->val) {
+                    return false;
+                }
+                if (temp1->left && temp2->left) {
+                    q1.push(temp1->left);
+                    q2.push(temp2->left);
+                }
+                else if (temp1->left || temp2->left) {
+                    return false;
+                }
+                if (temp1->right && temp2->right) {
+                    q1.push(temp1->right);
+                    q2.push(temp2->right);
+                }
+                else if (temp1->right || temp2->right) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
